00d87ccc01b778303e7f3cda7803d61c7a4c17cb
Swastik Thiramdas <110117201+swastikthiramdas@users.noreply.github.com>
2022-08-21 05:52:40 -0700

    notification bar hide

--- app/src/main/java/com/swastik/newsapp/MainActivity.kt
+++ app/src/main/java/com/swastik/newsapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.swastik.newsapp
 
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,12 +18,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
         GlobalScope.launch {
             fetchdata()
         }
-        
+
     }
 
     fun fetchdata() {
--- app/src/main/java/com/swastik/newsapp/SplashScrean.kt
+++ app/src/main/java/com/swastik/newsapp/SplashScrean.kt
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.Window
+import android.view.WindowManager
 
 class SplashScrean : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_splash_screan)
 
         Handler(Looper.getMainLooper()).postDelayed({
